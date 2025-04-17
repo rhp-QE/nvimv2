1304cfd8df30f95c488da9651977f423f7a44d4c
Ruan Huipeng <2767360276@qq.com>
2025-04-18 00:05:40 +0800

    修改 模式写切换快捷键

--- lua/user/settings.lua
+++ lua/user/settings.lua
@@ -5,7 +5,7 @@ local settings = {}
 settings["use_ssh"] = false
 
 -- 插入模式下，按 `jk` 退出到普通模式
-vim.keymap.set('i', 'jk', '<ESC>', { noremap = true, silent = true })
+vim.keymap.set('i', 'fd', '<ESC>', { noremap = true, silent = true })
 
 -- 主题
 settings["colorscheme"] = "vscode"
